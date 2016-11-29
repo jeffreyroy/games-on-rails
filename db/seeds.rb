4811bd2f832e51f4604cb0757b349c55c6021001
jeffreyroy <jeffrey_a_roy@yahoo.com>
2016-11-28 21:41:22 -0800

    add see file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create( name: "Joe" )
+
+SavedGame.create
+
+g = Annuvin.new
+g.export
